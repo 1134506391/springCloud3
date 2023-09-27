057fc77245423f7cf7eac6971a22d7a55fd00606
3hpyj <3hpyj@3h.net>
2023-09-27 16:15:02 +0800

    feat: 通过feign实现各模块之间的方法调用

--- member/src/main/java/com/example/springCloud3/member/controller/TestController.java
+++ member/src/main/java/com/example/springCloud3/member/controller/TestController.java
@@ -1,12 +1,22 @@
 package com.example.springCloud3.member.controller;
 
+import com.example.springCloud3.member.feign.BusinessFeign;
+import jakarta.annotation.Resource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
+    private static final Logger LOG = LoggerFactory.getLogger(TestController.class);
+
+    @Resource
+    BusinessFeign businessFeign;
     @GetMapping("/hello")
     public String hello() {
+        String businessHello = businessFeign.hello();
+        LOG.info("feign调用business方法:------"+businessHello);
         return "Hello World!";
     }
 }
