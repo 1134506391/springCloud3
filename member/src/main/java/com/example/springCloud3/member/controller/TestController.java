f3d752878f2898dfa828b91b59561551306549f2
3hpyj <3hpyj@3h.net>
2023-10-08 16:25:53 +0800

    feat:集成Nacos配置中心，实现动态刷新的功能

--- member/src/main/java/com/example/springCloud3/member/controller/TestController.java
+++ member/src/main/java/com/example/springCloud3/member/controller/TestController.java
@@ -4,12 +4,22 @@ import com.example.springCloud3.member.feign.BusinessFeign;
 import jakarta.annotation.Resource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RefreshScope
 public class TestController {
     private static final Logger LOG = LoggerFactory.getLogger(TestController.class);
+
+    @Value("${test.nacos}")
+    private String testNacos;
+
+    @Value("${test.nacos2}")
+    private String testNacos2;
+
 
     @Resource
     BusinessFeign businessFeign;
@@ -17,11 +27,12 @@ public class TestController {
     public String hello() {
         String businessHello = businessFeign.hello();
         LOG.info("feign调用business方法:------"+businessHello);
-        return "Hello World22!";
+//        return "Hello World22!";
+        return String.format("Hello %s!", testNacos);
     }
 
     @GetMapping("/login")
     public String login() {
-        return "login22";
+        return testNacos2;
     }
 }
